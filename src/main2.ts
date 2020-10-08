456ce5f5604bba51ca2b9d05e6473c923ebe33e2
jordonias <jordan@jordonias.com>
2020-10-08 10:35:50 -0500

    1 file modified

--- src/main2.ts
+++ src/main2.ts
@@ -3,9 +3,9 @@ import './polyfills';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { App2Module } from './app/feature.module/feature.module';
+import { FeatureModule } from './app/feature.module/feature.module';
 
-platformBrowserDynamic().bootstrapModule(App2Module).then(ref => {
+platformBrowserDynamic().bootstrapModule(FeatureModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
   if (window['ngRef']) {
     window['ngRef'].destroy();
