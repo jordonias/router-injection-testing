8b7e0d3d1bf3b227366a8c84d02b0ce89988fe53
jordonias <jordan@jordonias.com>
2020-10-08 11:12:33 -0500

    1 file modified

--- src/app/feature.module/feature.module.ts
+++ src/app/feature.module/feature.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -9,13 +10,13 @@ import { FooComponent } from './foo.component/foo.component';
 import { HomeComponent } from './home.component/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ],
+  imports:      [ CommonModule, FormsModule, ],
   declarations: [ BarComponent, FooComponent, HomeComponent ],
 })
 export class PublicModule { }
 
 @NgModule({
-  imports:      [ FeatureRoutingModule, PublicModule ],
+  imports:      [ BrowserModule, FeatureRoutingModule, PublicModule ],
   declarations: [ FeatureComponent ],
   bootstrap:    [ FeatureComponent ]
 })
